72427ccfec4d680620bf308509791e1fc7efe863
and_consul@outlook.com <and_consul@outlook.com>
2021-05-06 21:47:59 -0300

    build(setup):portifolio

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { AppProps } from "next/app";
+
+import GlobalStyle from '../styles/global';
+import { ThemeProvider } from "styled-components";
+import theme from "../styles/themes";
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+
+      <Component {...pageProps} />
+      <GlobalStyle />
+
+    </ThemeProvider>
+  )
+}
+
+export default MyApp
